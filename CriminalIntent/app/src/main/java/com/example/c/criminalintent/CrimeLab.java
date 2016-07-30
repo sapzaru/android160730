b0efd0b2a33c319a902640c192684be7be6b97f1
sapzaru <sapzaru@gmail.com>
2016-07-30 15:21:04 +0900

    - add singleton

---
+++ CriminalIntent/app/src/main/java/com/example/c/criminalintent/CrimeLab.java
@@ -0,0 +1,39 @@
+package com.example.c.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by c on 2016-07-30.
+ */
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private Context mAppContext;
+    private ArrayList<Crime> mCrimes;
+
+    private CrimeLab(Context c) {
+        mAppContext = c;
+        mCrimes = new ArrayList<Crime>();
+    }
+
+    public ArrayList<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime c : mCrimes) {
+            if (c.getId().equals(id))
+                return c;
+        }
+        return null;
+    }
+
+    public static CrimeLab get(Context c) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(c.getApplicationContext());
+        }
+        return sCrimeLab;
+    }
+}
