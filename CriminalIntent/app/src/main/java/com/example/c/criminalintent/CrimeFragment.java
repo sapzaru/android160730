176258f1ba92c702c7ca0a4a93b6748cfe841ab9
sapzaru <sapzaru@gmail.com>
2016-07-30 14:41:39 +0900

    -

--- CriminalIntent/app/src/main/java/com/example/c/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/example/c/criminalintent/CrimeFragment.java
@@ -9,6 +9,9 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 
@@ -18,6 +21,8 @@ import android.widget.EditText;
 public class CrimeFragment extends Fragment {
     Crime mCrime;
     EditText mTitleField;
+    Button mDateButton;
+    CheckBox mSolvedCheckBox;
 
     public CrimeFragment() {
         // Required empty public constructor
@@ -51,6 +56,17 @@ public class CrimeFragment extends Fragment {
 
             }
         });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
         return v;
     }
 
