d8939a7d9d36400f4f6a6e8e4f47b9ff0460514e
sapzaru <sapzaru@gmail.com>
2016-08-07 17:20:29 +0900

    add gallery item class

--- PhotoGallery/app/src/main/java/com/example/c/photogallery/FlickrFetchr.java
+++ PhotoGallery/app/src/main/java/com/example/c/photogallery/FlickrFetchr.java
@@ -1,15 +1,26 @@
 package com.example.c.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by c on 2016-08-07.
  */
 public class FlickrFetchr {
+    private static final String API_KEY = "9289b8daaf67da1c62623b2ff2c18e5f";
+
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -39,5 +50,49 @@ public class FlickrFetchr {
     // 바이트배열로 넘어오지만 스트링 이라는걸 알고 있을때
     public String getUrlString(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    public void fetchItems() {
+        ArrayList<GalleryItem> items = new ArrayList<>();
+
+        String url = Uri.parse("https://api.flickr.com/services/rest/")
+                .buildUpon()
+                .appendQueryParameter("method", "flickr.photos.getRecent")
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
+                .appendQueryParameter("extras", "url_s")    // url_s는 스몰사이즈 이미지
+                .build().toString();
+
+        try {
+            String jsonString = getUrlString(url);
+            Log.i("json", jsonString);
+
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItem(items, jsonBody);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseItem(List<GalleryItem> items, JSONObject jsonBody) throws JSONException {
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+        for (int i = 0; i < photoJsonArray.length(); i++) {
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+
+            if (photoJsonObject.has("url_s")) {
+                item.setUrl(photoJsonObject.getString("url_s"));
+            }
+
+            items.add(item);
+        }
     }
 }
--- PhotoGallery/app/src/main/java/com/example/c/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/example/c/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,14 @@
 package com.example.c.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.io.IOException;
 
 /**
  * Created by c on 2016-08-07.
@@ -23,10 +21,26 @@ public class PhotoGalleryFragment extends Fragment {
         return fragment;
     }
 
+    class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        // 쓰레드에서 직접 ui에 접근하면 크래쉬가 나는데
+        // AsyncTask는 쓰레드와 핸들러(UI쪽)가 합쳐진 개념으로
+        // ui쪽을 호출하여도 된다.
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // 쓰레드 내부라고 보면 된다.
+            new FlickrFetchr().fetchItems();
+            return null;
+        }
+    }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new FetchItemsTask().execute();
 
+        /*
+        // 진저브레드 이후에는 네트워크는 다른 쓰레드에서 해야함..
         FlickrFetchr f = new FlickrFetchr();
         try {
             String str = f.getUrlString("http://www.naver.com");
@@ -34,6 +48,7 @@ public class PhotoGalleryFragment extends Fragment {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        */
     }
 
     @Nullable
