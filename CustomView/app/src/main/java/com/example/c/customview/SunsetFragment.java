1c746db14b020dd76725f701a407bdbf1b3cb73d
sapzaru <sapzaru@gmail.com>
2016-08-14 15:22:01 +0900

    add ObjectAnimator

--- CustomView/app/src/main/java/com/example/c/customview/SunsetFragment.java
+++ CustomView/app/src/main/java/com/example/c/customview/SunsetFragment.java
@@ -1,16 +1,21 @@
 package com.example.c.customview;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 
 /**
  * Created by c on 2016-08-14.
  */
 public class SunsetFragment extends Fragment {
+    private View mSceneView, mSunView, mSkyView;
+    private int mBlueSkyColor, mSunsetSkyColor, mNightSkyColor;
+
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
     }
@@ -19,6 +24,38 @@ public class SunsetFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_sunset, container, false);
+        mSceneView = v;
+        mSunView = v.findViewById(R.id.sun);
+        mSkyView = v.findViewById(R.id.sky);
+
+        mBlueSkyColor = getResources().getColor(R.color.blue_sky);
+        mSunsetSkyColor = getResources().getColor(R.color.sunset_sky);
+        mNightSkyColor = getResources().getColor(R.color.night_sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
         return v;
+    }
+
+    private void startAnimation() {
+        float sunYStart = mSunView.getTop();    // 해의 위쪽이
+        float sunYEnd = mSkyView.getHeight();   // 하늘의 아래쪽으로
+
+        // 해
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(mSunView, "y", sunYStart, sunYEnd)
+                .setDuration(3000);
+        heightAnimator.setInterpolator(new AccelerateInterpolator());
+        heightAnimator.start();
+
+        ObjectAnimator sunsetSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor)
+                .setDuration(3000);
+        sunsetSkyAnimator.start();
+
     }
 }
